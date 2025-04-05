e0079796cd23a65ac2c8269124ceaa65ccc75219
BryanJames16 <bryan.james.ilaga.lds16@gmail.com>
2025-04-06 00:15:27 +0800

    base recheck for docker build

--- Core/test.mk
+++ Core/test.mk
@@ -1,5 +1,10 @@
 .PHONY: dotnet-test
 dotnet-test:
+	@if [ "$(DOTNET_SP_TEST_TOOL)" = "xunit" ]; then \
+		echo "Installing xUnit 3 templates..."; \
+		dotnet new install xunit.v3.templates; \
+		echo "Done installing xUnit 3 templates!"; \
+	fi
 	@echo "Performing unit testing..."
 	dotnet test $(DOTNET_SP_TEST_PATH) -v $(DOTNET_SP_TEST_VERBOSITY) $(DOTNET_SP_TEST_ADDITIONAL_FLAGS)
 	@echo "Unit test completed!"
--- pipeline.mk
+++ pipeline.mk
@@ -1,5 +1,6 @@
 # Include Core Files
 include Core/startup.mk
 include Core/build.mk
+include Core/publish.mk
 include Core/test.mk
 include Core/clean.mk
