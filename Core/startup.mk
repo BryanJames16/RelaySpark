4c928d9a48088203ea9d748e7d0eb7807bb7f485
BryanJames16 <bryan.james.ilaga.lds16@gmail.com>
2025-04-08 21:04:59 +0800

    Updated startup

--- Core/startup.mk
+++ Core/startup.mk
@@ -1,6 +1,12 @@
 .PHONY: startup
 startup:
-	@echo "You are using make for pipeline!"
+	@echo  "  ___     _           ___                _    "
+	@echo  " | _ \\___| |__ _ _  _/ __|_ __  __ _ _ _| |__ "
+	@echo  " |   / -_) / _\` | || \\__ \\ '_ \\/ _\` | '_| / / "
+	@echo  " |_|_\\___|_\\__,_|\\_, |___/ .__/\\__,_|_| |_\\_\\ "
+	@echo  "                 |__/    |_|                  "
+
+	make --version
 
 .PHONY: print-echo
 print-echo:
