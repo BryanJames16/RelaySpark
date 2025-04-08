c66764bfd0609863aa9a4357555807a627b44ce3
BryanJames16 <bryan.james.ilaga.lds16@gmail.com>
2025-04-08 21:10:30 +0800

    Added build commands

--- Core/build.mk
+++ Core/build.mk
@@ -1,3 +1,13 @@
+.PHONY: dotnet-build
+dotnet-build:
+	make _dotnet-build
+
+.PHONY: _dotnet-build
+_dotnet-build:
+	@echo "Performing dotnet build..."
+	dotnet build $(DOTNET_BUILD_SP_PATH) -v $(DOTNET_BUILD_VERBOSITY) $(DOTNET_BUILD_ADDITIONAL_PARAMETERS)
+	@echo "Completed dotbet build!"
+
 .PHONY: docker-build
 docker-build:
 	make _docker-build
