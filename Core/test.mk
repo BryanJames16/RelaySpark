ee569684f3aaefba1021d4822c07de66f4abe340
BryanJames16 <bryan.james.ilaga.lds16@gmail.com>
2025-04-08 22:57:35 +0800

    Added test documentation

--- Core/test.mk
+++ Core/test.mk
@@ -8,6 +8,14 @@
 # Security testing such as SAST will be covered on another file.
 ##
 
+##
+# @function     dotnet-clean
+# @brief        Job for cleaning .NET project or solution (.NET Core)
+# @param[in]    DOTNET_SP_TEST_TOOL                  Tools for .NET unit testing: `xunit`, `nunit`, and `mstest`.
+# @param[in]    DOTNET_TEST_SP_PATH                  Path where the project or the solution file is placed.
+# @param[in]    DOTNET_TEST_VERBOSITY                Verbosity of the build. Available potions are: `q[uiet]`, `m[inimal]`, `n[ormal]`, `d[etailed]`, and `diag[nostic]`
+# @param[in]    DOTNET_TEST_ADDITIONAL_FLAGS         Additional parameters to pass to `dotnet clean`.
+##
 .PHONY: dotnet-test
 dotnet-test:
 	make _dotnet-test
@@ -20,5 +28,5 @@ _dotnet-test:
 		echo "Done installing xUnit 3 templates!"; \
 	fi
 	@echo "Performing unit testing..."
-	dotnet test $(DOTNET_SP_TEST_PATH) -v $(DOTNET_SP_TEST_VERBOSITY) $(DOTNET_SP_TEST_ADDITIONAL_FLAGS)
+	dotnet test $(DOTNET_TEST_SP_PATH) -v $(DOTNET_TEST_VERBOSITY) $(DOTNET_TEST_ADDITIONAL_FLAGS)
 	@echo "Unit test completed!"
