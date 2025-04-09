d95ff91bf28dd75cc19d4db64d4a7686f16371ca
BryanJames16 <bryan.james.ilaga.lds16@gmail.com>
2025-04-09 10:56:32 +0800

    Updated values for trivy and gitleaks scanning

--- Core/security.mk
+++ Core/security.mk
@@ -7,6 +7,11 @@
 # This mk file contains job for seccurity applications.
 ##
 
+## ----------------------------------
+#  Trivy Scan
+## ----------------------------------
+
+
 ##
 # @function     trivy-scan
 # @brief        Wrapper for all trivy scans
@@ -32,6 +37,7 @@ trivy-scan:
 # @param[in]    TRIVY_IMAGE_SCAN_IMAGE_URL                  Full image name and tag for sccanning. You can also instead use `--input <TAR_FILE>` if you want to scan a tar file.
 # @param[in]    TRIVY_IMAGE_SCAN_OUTPUT_FORMAT              Output format for Trivy. Allowed values are: `table`, `json`, `template`, `sarif`, `cyclonedx`, `spdx`, `spdx-json`, `github`, and `cosign-vuln`.
 # @param[in]    TRIVY_IMAGE_SCAN_EXIT_CODE                  Exit code for trivy scan (1 for vulnerabilities detected, 0 for none)
+# @param[in]    TRIVY_IMAGE_SCAN_OUTPUT_FILE                Output file for trivy scan.
 # @param[in]    TRIVY_IMAGE_SCAN_ADDITIONAL_PARAMETERS      Any additional trivy scan parameters.
 ##
 .PHONY: trivy-image-scan
@@ -42,6 +48,7 @@ trivy-image-scan:
 _trivy-image-scan:
 	@echo "Performing trivy scan..."
 	trivy --version
+	trivy image $(TRIVY_IMAGE_SCAN_IMAGE_URL) --format $(TRIVY_IMAGE_SCAN_OUTPUT_FORMAT) --output $(TRIVY_IMAGE_SCAN_OUTPUT_FILE) --quiet $(TRIVY_IMAGE_SCAN_ADDITIONAL_PARAMETERS)
 	trivy image $(TRIVY_IMAGE_SCAN_IMAGE_URL) --format $(TRIVY_IMAGE_SCAN_OUTPUT_FORMAT) --exit-code $(TRIVY_IMAGE_SCAN_EXIT_CODE) $(TRIVY_IMAGE_SCAN_ADDITIONAL_PARAMETERS)
 	@echo "Completed trivy scan!"
 
@@ -51,6 +58,7 @@ _trivy-image-scan:
 # @param[in]    TRIVY_FS_SCAN_PATH                       Path where trivy will start scanning.
 # @param[in]    TRIVY_FS_SCAN_OUTPUT_FORMAT              Output format for Trivy. Allowed values are: `table`, `json`, `template`, `sarif`, `cyclonedx`, `spdx`, `spdx-json`, `github`, and `cosign-vuln`.
 # @param[in]    TRIVY_FS_SCAN_EXIT_CODE                  Exit code for trivy scan (1 for vulnerabilities detected, 0 for none)
+# @param[in]    TRIVY_FS_SCAN_OUTPUT_FILE                Output file for trivy scan.
 # @param[in]    TRIVY_FS_SCAN_ADDITIONAL_PARAMETERS      Any additional trivy scan parameters.
 ##
 .PHONY: trivy-fs-scan
@@ -61,6 +69,7 @@ trivy-fs-scan:
 _trivy-fs-scan:
 	@echo "Performing trivy scan..."
 	trivy --version
+	trivy fs $(TRIVY_FS_SCAN_PATH) --format $(TRIVY_FS_SCAN_OUTPUT_FORMAT) --output $(TRIVY_FS_SCAN_OUTPUT_FILE) --quiet $(TRIVY_FS_SCAN_ADDITIONAL_PARAMETERS)
 	trivy fs $(TRIVY_FS_SCAN_PATH) --format $(TRIVY_FS_SCAN_OUTPUT_FORMAT) --exit-code $(TRIVY_FS_SCAN_EXIT_CODE) $(TRIVY_FS_SCAN_ADDITIONAL_PARAMETERS)
 	@echo "Completed trivy scan!"
 
@@ -70,6 +79,7 @@ _trivy-fs-scan:
 # @param[in]    TRIVY_REPO_SCAN_URL                        Repository URL to scan (can be remote or local repo)
 # @param[in]    TRIVY_REPO_SCAN_OUTPUT_FORMAT              Output format for Trivy. Allowed values are: `table`, `json`, `template`, `sarif`, `cyclonedx`, `spdx`, `spdx-json`, `github`, and `cosign-vuln`.
 # @param[in]    TRIVY_REPO_SCAN_EXIT_CODE                  Exit code for trivy scan (1 for vulnerabilities detected, 0 for none)
+# @param[in]    TRIVY_REPO_SCAN_OUTPUT_FILE                Output file for trivy scan.
 # @param[in]    TRIVY_REPO_SCAN_ADDITIONAL_PARAMETERS      Any additional trivy scan parameters.
 ##
 .PHONY: trivy-repo-scan
@@ -80,5 +90,29 @@ trivy-repo-scan:
 _trivy-repo-scan:
 	@echo "Performing trivy scan..."
 	trivy --version
-	trivy fs $(TRIVY_REPO_SCAN_URL) --format $(TRIVY_REPO_SCAN_OUTPUT_FORMAT) --exit-code $9(TRIVY_REPO_SCAN_EXIT_CODE) $(TRIVY_REPO_SCAN_ADDITIONAL_PARAMETERS)
+	trivy repo $(TRIVY_REPO_SCAN_URL) --format $(TRIVY_REPO_SCAN_OUTPUT_FORMAT) --output $(TRIVY_REPO_SCAN_OUTPUT_FILE) --quiet $(TRIVY_REPO_SCAN_ADDITIONAL_PARAMETERS)
+	trivy repo $(TRIVY_REPO_SCAN_URL) --format $(TRIVY_REPO_SCAN_OUTPUT_FORMAT) --exit-code $(TRIVY_REPO_SCAN_EXIT_CODE) $(TRIVY_REPO_SCAN_ADDITIONAL_PARAMETERS)
 	@echo "Completed trivy scan!"
+
+## ----------------------------------
+#  Gitleaks Scan
+## ----------------------------------
+
+##
+# @function     gitleaks-dir-scan
+# @brief        Gitleaks Directory Scanning
+# @param[in]    GITLEAKS_DIR_SCAN_PATH                       Path where Gitleaks will scan for exposed secrets.
+# @param[in]    GITLEAKS_DIR_SCAN_OUTPUT_FORMAT              Output format for Gitleaks. Allowed values are: `json`, `csv`, `junit`, `sarif`, `template`.
+# @param[in]    GITLEAKS_DIR_SCAN_OUTPUT_FILE                Path and file name for report output
+# @param[in]    GITLEAKS_DIR_SCAN_EXIT_CODE                  Exit code for trivy scan (1 for vulnerabilities detected, 0 for none)
+# @param[in]    GITLEAKS_DIR_SCAN_ADDITIONAL_PARAMETERS      Any additional trivy scan parameters.
+##
+.PNONY: gitleaks-dir-scan
+gitleaks-dir-scan:
+	$(MAKE) _gitleaks-dir-scan
+
+.PHONY: _gitleaks-dir-scan
+_gitleaks-dir-scan:
+	@echo "Performing gitleaks scan..."
+	gitleaks dir $(GITLEAKS_DIR_SCAN_PATH) --report-format $(GITLEAKS_DIR_SCAN_OUTPUT_FORMAT) --report-path $(GITLEAKS_DIR_SCAN_OUTPUT_FILE) --exit-code $(GITLEAKS_DIR_SCAN_EXIT_CODE) -v $(GITLEAKS_DIR_SCAN_ADDITIONAL_PARAMETERS)
+	@echo "Completed gitleaks scan!"
