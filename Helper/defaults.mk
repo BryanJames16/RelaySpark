9979a7e731b72f183ce02e98e6204951001c15b1
BryanJames16 <bryan.james.ilaga.lds16@gmail.com>
2025-04-12 15:41:14 +0800

    Added base commands on defaults.mk

--- Helper/defaults.mk
+++ Helper/defaults.mk
@@ -2,6 +2,9 @@
 #  General Configurations
 ## ----------------------------------
 MAKE = make
+CONTAINER_COMMAND_BASE = docker compose run
+CONTAINER_COMMAND_PARAMETER = --rm
+CONTAINER_COMMAND_SERVICE = alpine
 
 ## ----------------------------------
 #  SECURITY
